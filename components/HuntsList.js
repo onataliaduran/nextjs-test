b10013950c473d3fdeef268023b4f2a57f44dcd1
Natalia Duran <o.nataliaduran@gmail.com>
2020-11-15 23:01:22 -0300

    initial commit

---
+++ components/HuntsList.js
@@ -0,0 +1,13 @@
+import styles from '../styles/HuntsList.module.css';
+
+
+const HuntsList = ({ title, client }) => {
+    return (
+        <div className={styles.card}>
+            <h2>{ title }</h2>
+            <p><strong>Company:</strong> { client }</p>
+        </div>   
+    )
+}
+
+export default HuntsList;
---
+++ pages/hunts/index.js
@@ -0,0 +1,33 @@
+import styles from '../../styles/Hunts.module.css';
+import HuntsList from '../../components/HuntsList';
+
+export default function Hunts({ hunts }) {
+
+    const URL = 'https://run.mocky.io/v3/a9260017-a896-4b8d-9247-5037f9e6f5a2';
+
+    return (
+        <>
+            <h1 className={styles.title}>Hunts List</h1>
+            <h2 className={styles.subTitle}>Mock API: { URL }</h2>
+            <div className={styles.listContainer}>
+                { 
+                    hunts.map( hunt => {
+                        return  <HuntsList
+                            title={hunt.title}
+                            client={hunt.client} />
+                    }) 
+                }
+            </div>
+        </>
+    )
+}
+
+Hunts.getInitialProps = () => {
+    return fetch('https://run.mocky.io/v3/a9260017-a896-4b8d-9247-5037f9e6f5a2')
+      .then(res => res.json())
+      .then(response => {
+        console.log(response);
+        const { hunts } = response;
+        return { hunts };
+        });
+}
